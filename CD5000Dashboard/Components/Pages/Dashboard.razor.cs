0b7c071be49b5969a9d5d7d330ab37861b0fc622
andrew76280@gmail.com <andrew76280@gmail.com>
2026-04-17 11:43:57 -0700

    Added tables to the dasboard completing the first task

--- CD5000Dashboard/Components/Pages/Dashboard.razor.cs
+++ CD5000Dashboard/Components/Pages/Dashboard.razor.cs
@@ -1,11 +1,18 @@
-﻿namespace CD5000Dashboard.Components.Pages
+﻿using CD5000Dashboard.Data.Models;
+
+namespace CD5000Dashboard.Components.Pages
 {
     public partial class Dashboard
     {
         private bool _loading = true;
         private int _transactionCount;
         private string? _errorMessage;
-        private IEnumerable<Data.DashboardRepository.ProductHourlyStat>? _hourlyData;
+
+        private IEnumerable<HourlyTransactionRow> _hourlyData = Enumerable.Empty<HourlyTransactionRow>();
+        private IEnumerable<MissingRfidRow> _missingRfid = Enumerable.Empty<MissingRfidRow>();
+        private IEnumerable<AvgDurationByVehicleRow> _avgDurationByVehicle = Enumerable.Empty<AvgDurationByVehicleRow>();
+        private IEnumerable<AvgDurationByUnitTrainRow> _avgDurationByUnitTrain = Enumerable.Empty<AvgDurationByUnitTrainRow>();
+        private IEnumerable<TopProductRow> _topProducts = Enumerable.Empty<TopProductRow>();
 
         protected override async Task OnInitializedAsync()
         {
@@ -13,6 +20,10 @@
             {
                 _transactionCount = await Repository.GetTransactionCountAsync();
                 _hourlyData = await Repository.GetTransactionsByHourAsync();
+                _missingRfid = await Repository.GetMissingRfidAsync();
+                _avgDurationByVehicle = await Repository.GetAverageDurationByVehicleAsync();
+                _avgDurationByUnitTrain = await Repository.GetAverageDurationByUnitTrainAsync();
+                _topProducts = await Repository.GetTopProductsAsync();
             }
             catch (Exception ex)
             {
---
+++ CD5000Dashboard/Data/Models/HourlyTransactionRow.cs
@@ -0,0 +1,8 @@
+﻿namespace CD5000Dashboard.Data.Models
+{
+    public class HourlyTransactionRow
+    {
+        public string? Hour { get; set; }
+        public int TransactionCount { get; set; }
+    }
+}
